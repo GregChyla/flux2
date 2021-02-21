26ffd091f21269e16506851260878371d545f568
Grzegorz Chyła <grzegorz.chyla@lppsa.com>
2021-02-21 12:59:26 +0100

    change webClient config

--- src/main/java/com/example/flux2/api/FluxApi.java
+++ src/main/java/com/example/flux2/api/FluxApi.java
@@ -6,32 +6,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @RequiredArgsConstructor
 public class FluxApi {
 
-    private final WebClient webClient = WebClient.builder().build();
-
+    private static final String GET_PATH = "/person/get";
+    private static final String GET_ALL_PATH = "/person/get/all";
     private final MeterService meterService;
-
+    private WebClient webClient;
     @Value("${flux1.address}")
     private String address;
+
+    @PostConstruct
+    private void setUpWebClient() {
+        webClient = WebClient.create(address);
+    }
+
     public Mono<Person> getOne() {
         meterService.start();
-        UriComponentsBuilder uri = UriComponentsBuilder.fromHttpUrl(address).path("/person/get");
-        Mono<Person> personResponse = webClient.get().uri(uri.build().toUriString()).retrieve().bodyToMono(Person.class);
+        Mono<Person> personResponse = webClient.get().uri(GET_PATH)
+                .retrieve()
+                .bodyToMono(Person.class);
         meterService.stop();
         return personResponse;
     }
 
     public Flux<Person> getAll() {
         meterService.start();
-        UriComponentsBuilder uri = UriComponentsBuilder.fromHttpUrl(address).path("/person/get/all");
-        Flux<Person> personResponse = webClient.get().uri(uri.build().toUriString()).retrieve().bodyToFlux(Person.class);
+        Flux<Person> personResponse = webClient.get().uri(GET_ALL_PATH)
+                .retrieve()
+                .bodyToFlux(Person.class);
         meterService.stop();
         return personResponse;
     }
--- src/main/java/com/example/flux2/controller/FluxController.java
+++ src/main/java/com/example/flux2/controller/FluxController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/flux1")
+@RequestMapping("/flux")
 @RequiredArgsConstructor
 public class FluxController {
 
